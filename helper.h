bf886fd5949a230f1a439baefef07d1a8ed70614
Abd-MOUSSAOUI <a.moussaoui05@gmail.com>
2018-11-10 00:32:25 +0100

    create shm and sem

--- helper.h
+++ helper.h
@@ -11,17 +11,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 #include <stdnoreturn.h>
+#include <errno.h>
+#include <stdbool.h>
+
 
 #define CODE_ERREUR 1
 #define SHMSZ 27
 
 char *prog;
 
+struct port *get_port(int shmid, bool readonly);
+
+struct port
+{
+    int dock_number;
+    char boat_name;
+};
 
 #endif /* helper_h */
